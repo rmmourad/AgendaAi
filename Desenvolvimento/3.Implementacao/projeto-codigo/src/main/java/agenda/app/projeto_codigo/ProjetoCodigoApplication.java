d996c9e9b20e8df8ea8a6603cb867b66940cb0df
Rhamza <rhamzamourad@gmail.com>
2024-05-27 22:42:12 -0300

    chore: project basic structure and files

---
+++ Desenvolvimento/3.Implementacao/projeto-codigo/src/main/java/agenda/app/projeto_codigo/ProjetoCodigoApplication.java
@@ -0,0 +1,13 @@
+package agenda.app.projeto_codigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCodigoApplication.class, args);
+	}
+
+}
